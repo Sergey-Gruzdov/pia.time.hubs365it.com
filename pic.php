e38ead2f6c010adcb698a6df6ea1cc9f37f36113
Sergey Gruzdov <sergey@gruzdov.dev>
2023-09-23 21:48:36 +0200

    just a whole rewrite

--- pic.php
+++ pic.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <link href="https://fonts.googleapis.com/css?display=swap&family=Shrikhand:400,400italic" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles_pic.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
+<meta http-equiv="refresh" content="3;url=http://time.hubs365it.com/index.php" />
 </head>
 <body>
     <div id="wrapper">
@@ -23,7 +24,7 @@
                             <div class="inner">
                                 <div>
                                     <p id="text01">
-                                        <u><a href="https://time.hubs365it.com/pic.php">♥</a></u>
+                                    <u><a href="https://time.hubs365it.com/pic.php">♥</a></u>
                                     </p>
                                 </div>
                             </div>
@@ -33,54 +34,57 @@
                         <div class="wrapper">
                             <div class="inner">
                                 <div>
-                                    <h1 id="text05">⠀ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏</h1>
+                                    <h1 id="text05"></h1>
                                     
-                                    <h1 id="text22">⠀ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏</h1>
+                                    <h1 id="text22"></h1>
                                 </div>
                                 <div>
                                     <h1 id="text16">
                                         Pia 
-                                        <em>! ♥</em>
+                                        <em>!</em>
                                     </h1>
                                     <h2 id="text10">
                                         <span class="p">
                                             <strong>
-                                                <em>she/her</em>
-                                                <br/>
+                                             well done!
                                             </strong>
-                                             A time schedule created by my awesome boyfriend!
-                                            <br/>
-                                            <a href="https://pyurhost.com">My boyfriends website.</a>
-                                            <br/>
                                         </span>
                                     </h2>
-                                    <h1 id="text18">⠀ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏ ͏͏͏͏͏͏</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="container08" class="container columns">
-                        <div class="wrapper">
-                            <div class="inner">
-                                <div>
-                                    <p id="text23">
-                                        <a href="yesterday.php">yesterday</a>
-                                    </p>
-                                </div>
-                                <div>
-                                    <p id="text24">
-                                        <a href="today.php">today</a>
-                                    </p>
-                                </div>
-                                <div>
-                                    <p id="text25">
-                                        <a href="tomorrow.php">tomorrow</a>
-                                    </p>
+                                    <h1 id="text18"></h1>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </header>
+                <section id="home-section">
+                    <div id="container03" class="container columns">
+                        <div class="wrapper">
+                            <div class="inner">
+                                <div>
+                                </div>
+                                <div>
+                                    <h1 id="text21"></h1>
+                                    <h1 id="text15">
+                                        <span class="p">
+                                        <div class="container_image">
+                                        <img src="image.jpg" alt="Centered Image" class="centered-image">
+                                        </div>
+                                        <br/>
+                                        </span>
+                                    </h1>
+                                    <h1 id="text04">
+                                        <span class="p">
+                                            <mark>
+                                            </mark>
+                                            <br/>
+                                        </span>
+                                    </h1>
+                                    <h1 id="text19"></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <footer id="footer">
                     <div id="container07" class="container default">
                         <div class="wrapper">
